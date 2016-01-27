2279707406390528e585e3f1cc83c07f552eaea2
Ana-Maria Ababei <anamarria.94@gmail.com>
2016-01-27 11:37:22 +0200

    bussiness :)

---
+++ QUIZ_ATM/QUIZ_ATM/bussiness/bussiness.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QUIZ_ATM.bussiness;
+using System.Data.Entity;
+using System.Data.EntityClient;
+using System.Data.Mapping;
+using System.Data.Entity.Infrastructure;
+using System.Data.EntityModel;
+
+namespace QUIZ_ATM.bussiness
+{
+    public class bussiness
+    {
+
+        public void cas()
+        {
+
+            Utilizatori ccc = new Utilizatori();
+            Admin adm = new Admin();
+            Statistica stat = new Statistica();
+
+            
+        }
+        
+           
+    }
+}
